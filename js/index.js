91b8f94cee6deba873dbb65f0009183fa265a62a
karthikch53 <karthik_rm@.lister.lan>
2013-06-06 17:31:43 +0530

    js changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-$('#editor').wysiwyg();
+initRTE();
 });
+
+function initRTE()
+{
+	tinymce.init({
+    selector: "textarea",
+    theme: "modern",   
+    plugins: [
+         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+         "save table contextmenu directionality emoticons template paste textcolor"
+   ],
+   toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist", 
+   style_formats: [
+        {title: 'Bold text', inline: 'b'},
+        {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+        {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+        {title: 'Example 1', inline: 'span', classes: 'example1'},
+        {title: 'Example 2', inline: 'span', classes: 'example2'},
+        {title: 'Table styles'},
+        {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+    ]
+ }); 
+}
+
+
+
